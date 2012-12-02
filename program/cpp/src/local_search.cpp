2e2f19778f177a8e0c68aa221a77fb347413a5b3
Jan Sochiera <jan.sochiera@gmail.com>
2012-12-02 18:09:01 +0100

    basic local search

--- program/cpp/src/local_search.cpp
+++ program/cpp/src/local_search.cpp
@@ -9,9 +9,23 @@
 typedef Individual P;
 
 
-P SimulatedAnnealing::operator () (Individual & a){
-
-
-
-	
+P SimulatedAnnealing::operator () (const P * Ind){
+	return *Ind;
 }
+
+
+P SinglePointOperator::operator () (const P * Ind){
+	P min(*Ind);
+	const int n = min.size();
+	for(int e = 0; e < n; e++){
+		for(int j = 0; j < n; j++){
+			P temp = insert(Ind, e, j);
+			if(p < min){
+				min = p;
+			}
+		}
+	}
+	return min;
+}
+
+
