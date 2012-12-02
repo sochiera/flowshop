aed8500aa37b4b6be4268960a9c51ee4567e3e26
Krzysztof Chrobak <krzychuch@gmail.com>
2012-12-02 18:19:14 +0000

    zmieniłem format danych na bardziej przyjazny dla c++

--- benchmark/process.py
+++ benchmark/process.py
@@ -10,25 +10,7 @@ for line in sys.stdin.readlines():
 
 text = ''.join(lines)
 numbers = map(int, text.split())
-
-# read problem description
-i = 0
-problems = []
-while i != len(numbers):
-  problem = {}
-  jobs = numbers[i]; i += 1
-  machines = numbers[i]; i += 2
-  problem['number'] = len(problems)
-  problem['jobs'] = jobs
-  problem['machines'] = machines
-  problem['upper_bound'] = numbers[i]; i += 1 
-  problem['lower_bound'] = numbers[i]; i += 1 
-  times = []
-  for machine in range(machines):
-    for job in range(jobs):
-      times.append(numbers[i])
-      i += 1
-  problem['times'] = times
-  problems.append(problem)
-
-sys.stdout.write(json.dumps(problems))
+sys.stdout.write(str(len(numbers)))
+for n in numbers:
+  sys.stdout.write(" ")
+  sys.stdout.write(str(n))
