68245eec98a0bb1d2a618fc7837ff68f60326774
Jan Sochiera <jan.sochiera@gmail.com>
2012-12-02 15:13:02 +0100

    test composition

---
+++ program/cpp/test/composition.cpp
@@ -0,0 +1,14 @@
+#include <crossover.h>
+#include <gtest/gtest.h>
+
+
+TEST(Composition, Crossover){
+  Individual mom("07243165");
+  Individual dad("72403156");
+
+  Composition c;
+  
+  Individual child = c.cross(mom, dad);
+  ASSERT_EQ(Individual("52304716"), child);
+}
+
